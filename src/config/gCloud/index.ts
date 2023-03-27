58f73e47ec9b3108b0355023c96892aae17c3113
kevin Castillo <kecoco16@gmail.com>
2023-03-27 11:20:14 -0600

    fix: private key from env vars

--- src/config/gCloud/index.ts
+++ src/config/gCloud/index.ts
@@ -1,7 +1,7 @@
 const config = {
   projectId: process.env.G_CLOUD_PROJECT_ID || '',
   clientEmail: process.env.G_CLOUD_CLIENT_EMAIL || '',
-  privateKey: process.env.G_CLOUD_PRIVATE_KEY || ''
+  privateKey: process.env.G_CLOUD_PRIVATE_KEY?.replace(/\\n/gm, '\n') || ''
 }
 
 export default config
